fdda6ea76e1d4717031faf17128c1058da84df69
ashwins <ashwin.shelke@coriolis.co.in>
2021-11-08 13:14:39 +0530

    added changes

--- restapi/accounts/api/urls.py
+++ restapi/accounts/api/urls.py
@@ -1,11 +1,8 @@
-from django.conf.urls import url, include
+from django.urls import path, include
 from django.contrib import admin
 from rest_framework_jwt.views import refresh_jwt_token, obtain_jwt_token # accounts app
 from .views import  RegisterAPIView
 
 urlpatterns = [
-    # url(r'^$', AuthAPIView.as_view(), name='login'),
-    url(r'^register/$', RegisterAPIView.as_view(), name='register'),
-    # url(r'^jwt/$', obtain_jwt_token),
-    # url(r'^jwt/refresh/$', refresh_jwt_token),
+	path('register/', RegisterAPIView.as_view(), name='register'),
 ]
